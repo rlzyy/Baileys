192b209b3d3c623b4339ef08fceeae1dc99365ac
DannTeam <110782241+DannOfficial@users.noreply.github.com>
2024-08-24 09:41:19 +0700

    Update State.ts

--- src/Types/State.ts
+++ src/Types/State.ts
@@ -1,29 +1,32 @@
-import { Contact } from './Contact'
+import {
+  Contact
+} from "./Contact"
 
-export type WAConnectionState = 'open' | 'connecting' | 'close'
+/** for (if connection === "open") at chat update (open, connecting, close) */
+export type WAConnectionState = "open" | "connecting" | "close"
 
 export type ConnectionState = {
-	/** connection is now open, connecting or closed */
-	connection: WAConnectionState
-	/** the error that caused the connection to close */
-	lastDisconnect?: {
-		error: Error | undefined
-		date: Date
-	}
-	/** is this a new login */
-	isNewLogin?: boolean
-	/** the current QR code */
-	qr?: string
-	/** has the device received all pending notifications while it was offline */
-	receivedPendingNotifications?: boolean
-	/** legacy connection options */
-	legacy?: {
-		phoneConnected: boolean
-		user?: Contact
-	}
-	/**
-	 * if the client is shown as an active, online client.
-	 * If this is false, the primary phone and other devices will receive notifs
-	 * */
-	isOnline?: boolean
+  /** connection is now open, connecting or closed */
+  connection: WAConnectionState
+  /** the error that caused the connection to close */
+  lastDisconnect?: {
+    error: Error | undefined
+    date: Date
+  }
+  /** is this a new login */
+  isNewLogin?: boolean
+  /** the current QR code */
+  qr?: string
+  /** has the device received all pending notifications while it was offline */
+  receivedPendingNotifications?: boolean /** true or false */
+  /** legacy connection options */
+  legacy?: {
+    phoneConnected: boolean
+    user?: Contact
+  }
+  /**
+  * if the client is shown as an active, online client.
+  * If this is false, the primary phone and other devices will receive notifs
+  * */
+  isOnline?: boolean
 }
